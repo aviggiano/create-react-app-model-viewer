24967e9d31085a920c0ad6e20b86212be0c1913e
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2021-08-12 19:26:48 -0300

    CRA with model-viewer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import '@google/model-viewer'
 import './App.css';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'model-viewer': ModelViewerJSX & React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> ;
+    }
+  }
+}
+
+interface ModelViewerJSX {
+  src: string
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <model-viewer src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
